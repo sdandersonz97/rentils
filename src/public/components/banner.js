9c02da099ed748cbd7ae7f783c7b55bc58ccd8ab
Steven Anderson <sdandersonz97@gmail.com>
2017-10-16 02:47:15 -0400

    fix: adding Link component

--- src/public/components/banner.js
+++ src/public/components/banner.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../styles/publicStyles.css'
+import { Link } from 'react-router-dom'
 const Banner = () => {
     return(
         <section className='banner'>
@@ -9,8 +10,8 @@ const Banner = () => {
                     <span className='title'> The platform for magnaments your rentals !!</span>
                 </h1>
                 <div style={{display: '-webkit-inline-box'}}>
-                    <a className='btn button' to='/login' >Login</a>
-                    <a className='btn button' to='/signup/company'>Get Started Now! </a>
+                    <Link className='btn button' to='/login' >Login</Link>
+                    <Link className='btn button' to='/signup/company'>Get Started Now! </Link>
                 </div> 
             </div>
         </section>
