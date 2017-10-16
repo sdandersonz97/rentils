769a5c2772ede5f7e614064a0bf2c52de63f0f3a
Steven Anderson <sdandersonz97@gmail.com>
2017-10-16 15:26:23 -0400

    fix: renamed component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './app'
+import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { applyMiddleware, createStore, compose } from 'redux'
