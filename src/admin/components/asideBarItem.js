a5ac21823f84fd2b4e74fcc21eab1fcf3b49ef85
Steven Anderson <sdandersonz97@gmail.com>
2017-10-16 16:01:36 -0400

    feat: aside bar item component

---
+++ src/admin/components/asideBarItem.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+const AsideBarItem = ({ route, icon, title, location }) => {
+    return(
+        <li className={location === route ? 'active' : ''}>
+            <Link className="nav-link" to={route}>
+                    <i className="material-icons">{icon}</i>
+                    <p className="nav-item">
+                        {title}
+                    </p>
+            </Link>
+        </li>
+    )
+}
+AsideBarItem.propTypes = {
+    route: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    icon: PropTypes.string
+}
+
+export default AsideBarItem
