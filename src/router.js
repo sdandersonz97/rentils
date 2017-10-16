61df70831454bcc8f06c0e1d9d5ded83331b91e9
Steven Anderson <sdandersonz97@gmail.com>
2017-10-16 15:26:37 -0400

    fix: router component

--- src/router.js
+++ src/router.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Route, Switch} from 'react-router-dom'
 import App from './public/App'
 import LoginForm from './auth/components/loginForm'
 import SignupForm from './auth/components/signupForm'
-
-
+import AdminRoutes from './admin/routes'
+import PrivateRoute from './auth/components/privateRoute'
 const Router = () =>{
     return (
         <Switch>
             <Route path='/signup' component={SignupForm}/>
             <Route path='/login' component={LoginForm} />
+            <PrivateRoute path="/company/:companyId/admin" component={AdminRoutes}/>
             <Route path='/' component={App}/>
         </Switch>
-    )
-    
+    ) 
 }
 export default Router
