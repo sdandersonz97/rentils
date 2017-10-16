55346b3a2595fd8f7b5c59e557d6524c6c06a053
Steven Anderson <sdandersonz97@gmail.com>
2017-10-16 05:18:14 -0400

    feat: feature proptypes

--- src/public/components/feature.js
+++ src/public/components/feature.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import '../styles/publicStyles.css'
+import PropTypes from 'prop-types'
 const Feature = ({img, title, description}) => {
     return(
-        <section className='featureContainer'>
+        <section className='featureContainer  col-sm-4 col-md-4'>
             <span className='icon'><img src={img} className='img-responsive imageFeature'/></span>
             <h3>{title}</h3>
             <p>{description}</p>
         </section>
     )
+}
+
+Feature.propTypes = {
+    img: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string
 }
 export default Feature
