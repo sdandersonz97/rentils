4a191fa198d2033a2b58ec96a043a79483b823b3
Steven Anderson <sdandersonz97@gmail.com>
2017-10-16 05:17:40 -0400

    fix: banner link

--- src/public/components/banner.js
+++ src/public/components/banner.js
@@ -11,7 +11,7 @@ const Banner = () => {
                 </h1>
                 <div style={{display: '-webkit-inline-box'}}>
                     <Link className='btn button' to='/login' >Login</Link>
-                    <Link className='btn button' to='/signup/company'>Get Started Now! </Link>
+                    <Link className='btn button' to='/signup'>Get Started Now! </Link>
                 </div> 
             </div>
         </section>
