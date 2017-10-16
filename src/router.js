d886b19ab4c2d00669722bb5159023e3d3d0c90d
Steven Anderson <sdandersonz97@gmail.com>
2017-10-16 05:14:02 -0400

    feat: new routes

--- src/router.js
+++ src/router.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import App from './public/App'
-
+import LoginForm from './auth/components/loginForm'
+import SignupForm from './auth/components/signupForm'
 class Router extends Component {
     render(){
         return (
             <Switch>
-                <Route path="/" component={App}/>
+                <Route path='/signup' component={SignupForm}/>
+                <Route path='/login' component={LoginForm} />
+                <Route path='/' component={App}/>
             </Switch>
         )
     }
