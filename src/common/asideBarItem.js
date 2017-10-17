c00dc41d447262b59946d12a1c150d1f80137865
Steven Anderson <sdandersonz97@gmail.com>
2017-10-17 10:44:29 -0400

    style: asideBarItem cleaning

--- src/common/asideBarItem.js
+++ src/common/asideBarItem.js
@@ -2,18 +2,17 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
-export const AsideBarItem = ({ route, icon, title, location }) => {
-    return(
-        <li className={location === route ? 'active' : ''}>
-            <Link className='nav-link' to={route}>
-                    <i className='material-icons'>{icon}</i>
-                    <p className='nav-item'>
-                        {title}
-                    </p>
-            </Link>
-        </li>
-    )
-}
+export const AsideBarItem = ({ route, icon, title, location }) => (
+    <li className={location === route ? 'active' : ''}>
+        <Link className='nav-link' to={route}>
+                <i className='material-icons'>{icon}</i>
+                <p className='nav-item'>
+                    {title}
+                </p>
+        </Link>
+    </li>
+)
+
 AsideBarItem.propTypes = {
     route: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
