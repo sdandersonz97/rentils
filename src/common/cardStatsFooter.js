e10da2cf749fa7343ca95ea6c7b3c76271a585c7
Steven Anderson <sdandersonz97@gmail.com>
2017-10-17 10:46:23 -0400

    feat: cardStatsFooter

---
+++ src/common/cardStatsFooter.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+export const CardStatsFooter = ({ urlFooter, children, link }) => (
+    <div className='card-footer'>
+        <div className='stats'>
+            {link 
+                ? <Link to={props.urlFooter}> {children} </Link>
+                : <p>{children}</p>}
+        </div>
+    </div> 
+)
+
+CardStatsFooter.propTypes = {
+    urlFooter: PropTypes.string,
+    link: PropTypes.bool,
+    children: PropTypes.element.isRequired,
+}
