437e80b75a16bc64d7cffeee3cb94c26b5db61f9
Steven Anderson <sdandersonz97@gmail.com>
2017-10-17 10:42:45 -0400

    feat: accoutingStatus component

---
+++ src/admin/components/accountingStatus.js
@@ -0,0 +1,16 @@
+import React from 'react'
+const AccountingStatus = () => {
+    return(
+        <div>
+            <div className='col-lg-3 col-md-6 col-sm-6'>
+                
+            </div>
+            <div className='col-lg-3 col-md-6 col-sm-6'>
+                
+            </div>
+            <div className='col-lg-3 col-md-6 col-sm-6'>
+                
+            </div>  
+        </div>
+    )
+}
