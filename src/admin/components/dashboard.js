0bc3449656cb1070f728ba085a1364dd27cdc949
Steven Anderson <sdandersonz97@gmail.com>
2017-10-17 10:46:54 -0400

    feat: dashboard

---
+++ src/admin/components/dashboard.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+const Dashboard = () => {
+    return(
+        <div className='content'>
+            <div className='container-fluid'>
+                <div className='row'>
+                   
+                </div>
+                <div className='row'>
+                    <div className='col-md-6'>
+
+                    </div>  
+                    <div className='col-md-6'>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Dashboard
