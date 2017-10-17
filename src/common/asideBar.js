b685bd209df371dd14cb134c4c5b496ea658a414
Steven Anderson <sdandersonz97@gmail.com>
2017-10-17 10:44:12 -0400

    style: aside bar cleaning

--- src/common/asideBar.js
+++ src/common/asideBar.js
@@ -1,23 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
-export const AsideBar = (props) =>{
-    return(
-        <aside className='sidebar' data-color='purple' data-image='../assets/img/sidebar-3.jpg'>
-            <div className='logo'>
-                <Link to={props.rootRoute} className='simple-text'>
-                    {props.header}
-                </Link>
-            </div>
-            <div className='sidebar-wrapper'>
-                <ul className='nav'>
-                    {props.children}
-                </ul>
-            </div>
-        </aside>
-        
-    )
-}
+export const AsideBar = ({ rootRoute, header, children, color }) => (
+    <aside className='sidebar' data-color={color} data-image='../assets/img/sidebar-3.jpg'>
+        <div className='logo'>
+            <Link to={rootRoute} className='simple-text'>
+                {header}
+            </Link>
+        </div>
+        <div className='sidebar-wrapper'>
+            <ul className='nav'>
+                {children}
+            </ul>
+        </div>
+    </aside>      
+)
 
 AsideBar.propTypes = {
     rootRoute: PropTypes.string,
