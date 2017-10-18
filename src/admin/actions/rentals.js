23f0989a180d83537dd8bb6aeb15527a8e55156b
Steven Anderson <sdandersonz97@gmail.com>
2017-10-18 11:33:46 -0400

    feat: fetch rental actions

--- src/admin/actions/rentals.js
+++ src/admin/actions/rentals.js
@@ -1,5 +1,14 @@
-import { ADD_COMPANY_RENTAL } from './types'
+import { ADD_COMPANY_RENTAL, FETCH_COMPANY_RENTALS } from './types'
 import { rentalsRef } from '../../utils/firebaseHelpers'
 
 export const addCompanyRental = (companyId, values) => () => 
     rentalsRef(companyId).push(values)
+
+
+export const fetchCompanyRentals = companyId => dispatch => 
+    rentalsRef(companyId).on('value', snap => 
+        dispatch({
+            type: FETCH_COMPANY_RENTALS,
+            rentals: snap.val()
+        })
+)   
