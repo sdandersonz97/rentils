6b016330bdcc66aaa0684ab9cbed1bcf50e3001e
Steven Anderson <sdandersonz97@gmail.com>
2017-10-19 01:21:43 -0400

    feat: rental list component

--- src/admin/containers/rentalList.js
+++ src/admin/containers/rentalList.js
@@ -1,33 +1,76 @@
-import React, { Component } from 'React'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Card, CardBody, CardHeader, Table, TableBody, TableHeader } from '../../common'
+import { Card, CardBody, CardHeader, Table, TableBody, TableHeader, DropdownButton } from '../../common'
 import { withRouter } from 'react-router-dom'
 import { fetchCompanyRentals } from '../actions/rentals'
 
-class RentalList extends Component {
-    renderRows = () => {}
+class RentalList extends Component { 
+    state={
+        filter: 'all'
+    }
+    componentDidMount(){
+        const { companyId } = this.props.match.params
+        const { fetchCompanyRentals, rentalList } = this.props
+        Object.keys(rentalList).length === 0 && fetchCompanyRentals(companyId)
+    }
+    onClickChangeFilter = filter => {
+        this.setState({
+            filter
+        })
+    }
+    renderOptions = () => [
+        { name:'Rented', onClick: () => this.onClickChangeFilter('rented') },
+        { name:'Available', onClick: () => this.onClickChangeFilter('available') },
+        { name:'All', onClick: () => this.onClickChangeFilter('all') }
+    ]
+    renderRows = rentalId => {
+        const { rentalList } = this.props
+        return(
+            <tr key={rentalId}>
+                <td>
+                    {rentalList[rentalId].cod}
+                </td>
+                <td>
+                    {rentalList[rentalId].address}
+                </td>
+                <td>
+                    {rentalList[rentalId].available ? 'AVAILABLE' : 'RENTED'}
+                </td>
+                <td>
+                    {rentalList[rentalId].cost}
+                </td>
+                <td>
+                    {rentalList[rentalId].description}
+                </td>
+                <td>
+                    {rentalList[rentalId].min} - {rentalList[rentalId].max}
+                </td>
+            </tr>
+        )
+    }
+    filterRentalList = () => {
+        const { rentalList } = this.props
+        const { filter } = this.state
+        return filter === 'all' 
+        ? Object.keys(rentalList)
+        : filter === 'available' 
+            ? Object.keys(rentalList).filter(rental => rentalList[rental].available )
+            : Object.keys(rentalList).filter(rental => !rentalList[rental].available )
+    }
     render(){
+        const { rentalList } = this.props
         return( 
             <Card>
-            <CardHeader title="Rentals" category="All the rentals "/>                               
+            <CardHeader title='Rentals' category='All the rentals'/>                               
             <CardBody>
+                <DropdownButton 
+                    title='filter'
+                    options={this.renderOptions()}
+                />
                 <Table>
-                    <TableHeader titles={['COD','ADDRESS','AVAIBLE','COST','DESCRIPTION']}/>
+                    <TableHeader titles={['COD','ADDRESS','DISPONIBILITY','COST','DESCRIPTION','RANGE']}/>
                     <TableBody>
-                    <tr key={rentalsData.id}>
-                        <td>
-                           Hola 
-                        </td>
-                        <td>
-                            Hola 
-                        </td>
-                        <td>
-                            Hola
-                        </td>
-                        <td>
-                            Hola
-                        </td>
-                    </tr>
+                        {this.filterRentalList().map(this.renderRows)}
                     </TableBody>
                 </Table>
             </CardBody>
--- src/admin/routes.js
+++ src/admin/routes.js
@@ -2,12 +2,14 @@ import React from 'react'
 import AdminApp from './App'
 import Dashboard from './components/dashboard'
 import RentalCreate from './components/rentalCreate'
+import RentalList from './containers/rentalList'
 import PrivateRoute from '../auth/components/privateRoute'
 const Routes = () => {
     return (
         <AdminApp>
             <PrivateRoute path='/company/:companyId/admin/dashboard' component={Dashboard} />
             <PrivateRoute path='/company/:companyId/admin/rentals/new' component={RentalCreate} />
+            <PrivateRoute exact path='/company/:companyId/admin/rentals' component={RentalList} />
         </AdminApp>
     )
 }
