7c3036e81bd52719adc1ec69eb28e1afe451d95f
Steven Anderson <sdandersonz97@gmail.com>
2017-10-19 14:48:03 -0400

    feat: helper to get an alert giving a days until the next payment

---
+++ src/utils/helpers.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const info = [7,4]
+const warning = [3,0]
+const alert = -1
+const errorStyle = color => {
+    return {
+        fontSize: 18,
+        position: 'relative',
+        top: 2,
+        left: 20,
+        color
+    }
+}
+const getAlert = days => {
+    return days <= info[0] && days >= info[1] 
+        ? <i className="material-icons" style={errorStyle('#19c6f4')}>error</i>
+        : days <= warning[0] && days >= warning[1]
+            ? <i className="material-icons" style={errorStyle('#f6ab2b')}>error</i>
+            : days <= alert && <i className="material-icons" style={errorStyle('#ea432c')}>error</i>
+}
+export const dayLeft = paymentDate => {
+    const oneDay = 1000*60*60*24
+    const days = Math.floor(( Date.parse(paymentDate) -  Date.now()) / oneDay)
+    return <span>{days} Days left {days <= info[0] && getAlert(days)} </span>
+}
