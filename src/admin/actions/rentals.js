81034a3656e940e8d91464d60381e9bebc054044
Steven Anderson <sdandersonz97@gmail.com>
2017-10-19 12:21:02 -0400

    feat: fetch rental action

--- src/admin/actions/rentals.js
+++ src/admin/actions/rentals.js
@@ -1,16 +1,22 @@
-import { FETCH_COMPANY_RENTALS } from './types'
+import { FETCH_COMPANY_RENTALS, FETCH_COMPANY_RENTAL } from './types'
 import { rentalsRef } from '../../utils/firebaseHelpers'
+
 export const addCompanyRental = (companyId, values, isRented) => () => {
     const RentalId = rentalsRef(companyId).push().key 
     rentalsRef(companyId).child(RentalId).set(values)
     return RentalId
 }
 
+export const fetchCompanyRental = (companyId, rentalId) => dispatch =>
+    rentalsRef(companyId).child(rentalId).on('value', snap =>
+        dispatch({
+            type: FETCH_COMPANY_RENTAL,
+            rental: snap.val() ? snap.val() : {}
+        }))
 
 export const fetchCompanyRentals = companyId => dispatch => 
     rentalsRef(companyId).on('value', snap => 
         dispatch({
             type: FETCH_COMPANY_RENTALS,
-            rentals: snap.val()
-        })
-)   
+            rentals: snap.val() ? snap.val() : {}
+        })) 
--- src/admin/reducers/rentals.js
+++ src/admin/reducers/rentals.js
@@ -1,11 +1,12 @@
 import _ from 'lodash'
-import { FETCH_COMPANY_RENTALS } from '../actions/types'
+import { FETCH_COMPANY_RENTALS, FETCH_COMPANY_RENTAL } from '../actions/types'
 
 
 const INITIAL_STATE = {
     rentalList: {},
     rentalsTotal: 0,
     rentalsAvailable: 0,
+    selectedRental: {}
 }
 
 export default (state=INITIAL_STATE, action) => {
@@ -16,6 +17,11 @@ export default (state=INITIAL_STATE, action) => {
                 rentalList: action.rentals,
                 rentalsTotal: _.size(action.rentals),
                 rentalsAvailable: _.size(_.filter(action.rentals, rental => rental.available))
+            }
+        case FETCH_COMPANY_RENTAL:
+            return {
+                ...state,
+                selectedRental: action.rental
             }
         default:
             return state
