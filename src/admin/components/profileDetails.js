94ec743a579099af497f84f034e124298ca91f81
Steven Anderson <sdandersonz97@gmail.com>
2017-10-20 01:07:55 -0400

    feat: profile details wrapper component

---
+++ src/admin/components/profileDetails.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ProfileInfo from '../containers/profileInfo'
+const ProfileDetails = () => {
+    return(
+        <div className="content">
+            <div className="container-fluid">    
+                   <ProfileInfo/>
+            </div>
+        </div>
+    )
+}
+
+export default ProfileDetails
