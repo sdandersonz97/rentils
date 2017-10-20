e6a51ae850534e197fc988df1c8133b2aa61a785
Steven Anderson <sdandersonz97@gmail.com>
2017-10-20 00:19:14 -0400

    feat: employees component done

--- src/admin/containers/employeesList.js
+++ src/admin/containers/employeesList.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Card, CardBody, CardHeader, Table, TableBody, TableHeader, DropdownButton } from '../../common'
 import { withRouter, Link } from 'react-router-dom'
-
+import { fetchCompanyEmployees } from '../actions/employees'
 class EmployeesList extends Component { 
     state={
         filter: 'all'
     }
     componentDidMount(){
         const { companyId } = this.props.match.params
-    
+        this.props.fetchCompanyEmployees(companyId)
     }
     onClickChangeFilter = filter => {
         this.setState({
@@ -17,38 +17,57 @@ class EmployeesList extends Component {
         })
     }
     renderOptions = () => [
-        { name:'best', onClick: () => this.onClickChangeFilter('best') },
-        { name:'worst', onClick: () => this.onClickChangeFilter('worst') },
+        { name:'Incomes', onClick: () => this.onClickChangeFilter('incomes') },
+        { name:'Expenses', onClick: () => this.onClickChangeFilter('expenses') },
+        { name:'Rentals', onClick: () => this.onClickChangeFilter('rentals') },
+        { name:'Rents', onClick: () => this.onClickChangeFilter('rents') },
         { name:'All', onClick: () => this.onClickChangeFilter('all') }
     ]
     renderRows = uid => {
-        const { employeeList } = this.props
+        const { employeesList } = this.props
         const { companyId } = this.props.match.params
         return(
             <tr key={uid}> 
                 <td>
-                    {employeeList[uid].id}
+                    {employeesList[uid].cod}
                 </td>
                 <td>
-                    <Link to={`/company/${companyId}/admin/employees/employee/${uid}`}>{employeeList[uid].fullname}</Link>
+                    <Link to={`/company/${companyId}/admin/employees/employee/${uid}`}>{employeesList[uid].fullname}</Link>
                 </td>
                 <td>
-                    {employeeList[uid].incomes}
+                    ${employeesList[uid].incomes}
                 </td>
                 <td>
-                    {employeeList[uid].expenses}
+                    ${employeesList[uid].expenses}
                 </td>
                 <td>
-                    {employeeList[uid].rentals}
+                    {employeesList[uid].rentals}
                 </td>
                 <td>
-                    {employeeList[uid].rents}
+                    {employeesList[uid].rents}
                 </td>
             </tr>
         )
     }
-    filterRentalList = () => {
-      
+    getBestEmployee = () => {
+        
+    }
+    filterEmployeesList = () => {
+        const {  employeesList } = this.props
+        const { filter } = this.state
+        const employeesListKeys = Object.keys(employeesList)
+        switch(filter){
+            case 'all':
+                return employeesListKeys
+            case 'incomes':
+                return employeesListKeys.sort((a,b) =>  employeesList[b].incomes - employeesList[a].incomes)
+            case 'expenses':
+                return employeesListKeys.sort((a,b) => employeesList[b].expenses - employeesList[a].expenses)
+            case 'rentals':
+                return employeesListKeys.sort((a,b) => employeesList[b].rentals - employeesList[a].rentals)
+            case 'rents':
+                return employeesListKeys.sort((a,b) => employeesList[b].rents - employeesList[a].rents)
+        }
     }
     render(){
         const { companyId } = this.props.match.params
@@ -63,26 +82,7 @@ class EmployeesList extends Component {
                 <Table>
                     <TableHeader titles={['COD','FULL NAME','INCOMES','EXPENSES','RENTALS','RENTS']}/>
                     <TableBody>
-                    <tr> 
-                        <td>
-                           AB4
-                        </td>
-                        <td>
-                            <Link to={`/company/${companyId}/admin/employees/employee/qwwe`}>STEVEN ANDERSON</Link>
-                        </td>
-                        <td>
-                            $1000
-                        </td>
-                        <td>
-                            $200
-                        </td>
-                        <td>
-                            10
-                        </td>
-                        <td>
-                            5
-                        </td>
-                    </tr>
+                    {this.filterEmployeesList().map(this.renderRows)}
                     </TableBody>
                 </Table>
             </CardBody>
@@ -93,7 +93,7 @@ class EmployeesList extends Component {
 
 const mapStateToProps = ({ employees }) => {
     return {
-        employeeList: employees.employeesList
+        employeesList: employees.employeesList
     }
 }
-export default withRouter(connect()(EmployeesList))
+export default withRouter(connect(mapStateToProps,{ fetchCompanyEmployees })(EmployeesList))
