2b2a3341325cf3f58c232f967a56b72196672639
Steven Anderson <sdandersonz97@gmail.com>
2017-10-20 23:14:49 -0400

    feat: list minified to select employees

---
+++ src/admin/components/employeeListMin.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Card, CardBody, CardHeader, Table, TableBody, TableHeader } from '../../common'
+
+const EmployeeListMin = ({ render, onClickChangeScreen, onSubmitAssignment }) => { 
+    return( 
+        <Card>
+        <CardHeader title='Employees' category='All the Employees'/>                               
+        <CardBody>
+            <Table>
+                <TableHeader titles={['SELECT','COD','FULL NAME']}/>
+                <TableBody>
+                    { render() }
+                </TableBody>
+            </Table>
+            <button className='btn btn-primary' onClick={() => onClickChangeScreen('rentals')}>Back</button>
+            <button className='btn btn-primary' onClick={onSubmitAssignment}>Assign</button>
+        </CardBody>
+        </Card>
+    )
+}
+
+
+export default EmployeeListMin
