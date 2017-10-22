d76f600bcbb5d840191d66ee0fe2cfbe72374c65
Steven Anderson <sdandersonz97@gmail.com>
2017-10-22 05:19:40 -0400

    feat: creating an activy when a rent is maked

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import { FETCH_COMPANY_EMPLOYEE } from './types'
-import { employeesRef, rentsRef, rentalsRef, rootRef } from '../utils/firebaseHelpers'
+import { employeesRef, rentsRef, rentalsRef, rootRef, employeeActivity } from '../utils/firebaseHelpers'
 
 
 export const fetchCompanyEmployee = (companyId, uid) => dispatch => 
@@ -15,10 +15,13 @@ export const addCompanyRent = (companyId, values) =>  {
     updates[`/companies/${companyId}/rentals/${values.rentalId}/available/`] = false
     employeesRef(companyId).child(`${values.uid}/rents`).once('value', snap => {
         updates[`/companies/${companyId}/employees/${values.uid}/rents`] = 1 + snap.val()
-    }
-    ).then(() => rentsRef(companyId).child(values.rentalId).set(values)
+    })
+    .then(() => rentsRef(companyId).child(values.rentalId).set(values)
         .then(() => rootRef().update(updates))
-        .catch(er => console.log(er))
-    )
+        .catch(er => console.log(er)))
+    .then(() => addEmployeeActivity())
     .catch(er => console.log(er))
 }
+
+const addEmployeeActivity = (companyId, uid, activity) => 
+    employeeActivity(companyId, uid).push(activity)
