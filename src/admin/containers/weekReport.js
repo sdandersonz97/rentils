c3834fc8fc1bef1e9084a1c8d37343018d742945
Steven Anderson <sdandersonz97@gmail.com>
2017-10-23 16:44:01 -0400

    feat: adding employee name to the report

--- src/admin/containers/weekReport.js
+++ src/admin/containers/weekReport.js
@@ -1,40 +1,46 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
-import { fetchCompanyIncomes } from '../actions/accounting' 
+import { fetchCompanyIncomes, fetchCompanyExpenses } from '../actions/accounting'
+import { fetchCompanyEmployees } from '../actions/employees' 
 import { Table, TableBody, TableHeader } from '../../common'
 
 class WeekReport extends Component {
     componentDidMount(){
         const { companyId } = this.props.match.params
         this.props.fetchCompanyIncomes(companyId)
+        this.props.fetchCompanyExpenses(companyId)
+        this.props.fetchCompanyEmployees(companyId)
     }
     filter = type => {
         const { incomes, expenses } = this.props
         const weekAgo = 1000 * 60 * 60* 24 * 7
         const keys = type === 'incomes' 
             ? Object.keys(incomes).filter(income => incomes[income].timestamp  >= Date.now() - weekAgo)
-            : Object.keys(expenses).filter(expenses => expenses[expenses].timestamp  >= Date.now() - weekAgo)
+            : Object.keys(expenses).filter(expense => expenses[expense].timestamp  >= Date.now() - weekAgo)
 
         return keys
     }
     renderPaymentsRows = incomesId => {
-        const { incomes } = this.props
+        const { incomes, employees } = this.props
         return(
             <tr key={incomesId}>
                 <td>${incomes[incomesId].mount}</td>
                 <td>{incomes[incomesId].quantity}</td>            
                 <td>{incomes[incomesId].tenant}</td>
+                <td>{employees[incomes[incomesId].uid] ? employees[incomes[incomesId].uid].fullname : ''}</td>
+                <td></td>
             </tr>
         )
     }
     renderExpensesRows = expensesId => {
-        const { expenses } = this.props
+        const { expenses, employees } = this.props
         return(
             <tr key={expensesId}>
                 <td>${expenses[expensesId].mount}</td>
                 <td>{expenses[expensesId].description}</td>            
-                <td>{expenses[expensesId].uid}</td>
+                <td>{employees[expenses[expensesId].uid] ? employees[expenses[expensesId].uid].fullname : ''}</td>
+                <td></td>
             </tr>
         )
     }
@@ -52,12 +58,14 @@ class WeekReport extends Component {
                 <div className='col-md-12'>
                     <h4 className='text-center'>PAYMENTS</h4>
                     <Table>
-                        <TableHeader  titles={['MOUNT','MONTHS PAID', 'TENANT']}/>
+                        <TableHeader  titles={['MOUNT', 'MONTHS PAID', 'TENANT','EMPLOYEE', 'TOTAL']}/>
                         <TableBody>
                             {this.filter('incomes').map(this.renderPaymentsRows)}
                             <tr>
                                 <td>TOTAL:</td>
-                                <td></td>            
+                                <td></td>  
+                                <td></td>      
+                                <td></td>       
                                 <td>${this.reduceToTotal('incomes')}</td>
                             </tr>
                         </TableBody>
@@ -66,12 +74,13 @@ class WeekReport extends Component {
                 <div className='col-md-12'>
                     <h4 className='text-center'>EXPENSES</h4>
                     <Table>
-                        <TableHeader  titles={['MOUNT', 'DESCRIPTION', 'EMPLOYEE']}/>
+                        <TableHeader  titles={['MOUNT', 'DESCRIPTION', 'EMPLOYEE', 'TOTAL']}/>
                         <TableBody>
                             {this.filter('expenses').map(this.renderExpensesRows)}
                             <tr>
                                 <td>TOTAL:</td>
-                                <td></td>            
+                                <td></td>          
+                                <td></td>  
                                 <td>${this.reduceToTotal('expenses')}</td>
                             </tr>
                         </TableBody>
@@ -82,10 +91,11 @@ class WeekReport extends Component {
         )
     }
 }
-const mapStateToProps = ({ incomes, expenses }) => {
+const mapStateToProps = ({ incomes, expenses, employees }) => {
     return{
         incomes,
-        expenses
+        expenses,
+        employees: employees.employeesList
     }
 }
-export default withRouter(connect(mapStateToProps, { fetchCompanyIncomes })(WeekReport))
+export default withRouter(connect(mapStateToProps, { fetchCompanyIncomes, fetchCompanyExpenses, fetchCompanyEmployees })(WeekReport))
