417977f5f9ddca4cb481a850c990c8a8f82b6e97
Steven Anderson <sdandersonz97@gmail.com>
2017-10-28 23:10:09 -0400

    feat: action to complete a payment note

--- src/employee/actions/operations.js
+++ src/employee/actions/operations.js
@@ -20,14 +20,32 @@ export const addCompanyIncome = (companyId, values) => {
 }
 
 export const addPaymentNote = (companyId, values) => {
+    const paymentNoteId = paymentNoteRef(companyId).push().key
     values.mount = parseFloat(values.mount)
     values.days = parseInt(values.days)
-    paymentNoteRef(companyId).push(values)
+    values.valid = true
+    paymentNoteRef(companyId).child(paymentNoteId).set(values)
     .then(() => addEmployeeActivity(companyId, values.uid, { 
         type: 'PAYMENT-NOTE', 
         message: `Payment Note from ${values.tenant} of $${values.mount} because ${values.description}`}))
+    .then(() => addPaymentNoteToRent(companyId, values.rentalId, paymentNoteId))
+    .then(() => updatePaymentDateByDays(companyId, values.rentalId, values.days))
 }
-
+export const completePaymentNote = (companyId, paymentNoteId) => {
+    paymentNoteRef(companyId).child(paymentNoteId).update({ valid:false })
+    .then(() => paymentNoteRef(companyId).child(paymentNoteId).once('value', snap => {
+        addCompanyIncome(companyId, {
+            mount: snap.val().mount + snap.val().rest, 
+            quantity:1, 
+            rentalId: snap.val().rentalId, 
+            timestamp: Date.now(), 
+            tenant: snap.val().tenant,
+            uid:snap.val().uid,
+            description: `Completed in two pays 1-$${snap.val().mount}, 2-$${snap.val().rest}` 
+        })
+        addPaymentNoteToRent(companyId, snap.val().rentalId, false)}
+    ))
+}
 export const fetchPaymentsNotes = (companyId, uid) => dispatch => 
     paymentNoteRef(companyId).orderByChild('uid').equalTo(uid).on('value', snap => 
         dispatch({
@@ -38,6 +56,13 @@ export const fetchPaymentsNotes = (companyId, uid) => dispatch =>
 const updatePaymentDate = (companyId, rentalId, months) => rentsRef(companyId).child(rentalId)
     .once('value', snap =>  
         rentsRef(companyId).child(rentalId).update({ paymentDate: snap.val().paymentDate + 1000 * 60 * 60 * 24 * 30 * months }))
+
+const updatePaymentDateByDays = (companyId, rentalId, days) => rentsRef(companyId).child(rentalId)
+.once('value', snap =>  
+    rentsRef(companyId).child(rentalId).update({ paymentDate: snap.val().paymentDate + 1000 * 60 * 60 * 24 * days }))
+
+const addPaymentNoteToRent = (companyId, rentalId, paymentNote) => rentsRef(companyId).child(rentalId).update({ paymentNote }) 
+
 const updateAccountingCompany = (companyId, mount, type) => accountingRef(companyId).child(type)
     .transaction(transaction => transaction += Number(mount))
 
