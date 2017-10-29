1d750512e2043200c7180cbe5fe8f3b3bc499636
Steven Anderson <sdandersonz97@gmail.com>
2017-10-28 23:09:06 -0400

    fix: adding a field payment note when a rent is created

--- src/actions/index.js
+++ src/actions/index.js
@@ -14,6 +14,7 @@ export const addCompanyRent = (companyId, values) =>  {
     const updates = {}
     values.paymentDate = Date.parse(values.paymentDate)
     values.price = parseFloat(values.price)
+    values.paymentNote = false
     updates[`/companies/${companyId}/rentals/${values.rentalId}/available/`] = false
     employeesRef(companyId).child(`${values.uid}/rents`).once('value', snap => {
         updates[`/companies/${companyId}/employees/${values.uid}/rents`] = 1 + snap.val()
