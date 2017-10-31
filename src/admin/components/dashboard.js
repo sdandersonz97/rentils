8d4ce02a91b087212a398cc90ea0f731ca6da58c
Steven Anderson <sdandersonz97@gmail.com>
2017-10-30 23:57:30 -0400

    feat: employees status

--- src/admin/components/dashboard.js
+++ src/admin/components/dashboard.js
@@ -2,6 +2,7 @@ import React from 'react'
 import AccountingStatus from '../containers/accountingStatus'
 import RentalsStatus from '../containers/rentalsStatus'
 import PaymentsChart from './paymentsChart'
+import EmployeesStatus from '../containers/employeesStatus'
 const Dashboard = () => {
     return(
         <div className='content'>
@@ -9,6 +10,7 @@ const Dashboard = () => {
                 <div className='row'>
                    <AccountingStatus/>
                    <RentalsStatus/>
+                   <EmployeesStatus/>
                 </div>
                 <div className='row'>
                     <div className='col-md-6'>
--- src/admin/components/employeeListMin.js
+++ src/admin/components/employeeListMin.js
@@ -4,17 +4,17 @@ import { Card, CardBody, CardHeader, Table, TableBody, TableHeader } from '../..
 const EmployeeListMin = ({ render, onClickChangeScreen, onSubmitAssignment }) => { 
     return( 
         <Card>
-        <CardHeader title='Employees' category='All the Employees'/>                               
-        <CardBody>
-            <Table>
-                <TableHeader titles={['SELECT', 'COD', 'FULL NAME', 'RENTALS']}/>
-                <TableBody>
-                    { render() }
-                </TableBody>
-            </Table>
-            <button className='btn btn-primary' onClick={onClickChangeScreen}>Back</button>
-            <button className='btn btn-primary' onClick={onSubmitAssignment}>Assign</button>
-        </CardBody>
+            <CardHeader title='Employees' category='All the Employees'/>                               
+            <CardBody>
+                <Table>
+                    <TableHeader titles={['SELECT', 'COD', 'FULL NAME', 'RENTALS']}/>
+                    <TableBody>
+                        { render() }
+                    </TableBody>
+                </Table>
+                <button className='btn btn-primary' onClick={onClickChangeScreen}>Back</button>
+                <button className='btn btn-primary' onClick={onSubmitAssignment}>Assign</button>
+            </CardBody>
         </Card>
     )
 }
