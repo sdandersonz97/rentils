08713c7168f3df190b9a69e7c5215bba6d2657a9
Steven Anderson <sdandersonz97@gmail.com>
2017-10-31 01:08:02 -0400

    feat: update accounting rental

--- src/employee/actions/operations.js
+++ src/employee/actions/operations.js
@@ -1,9 +1,10 @@
-import { expensesRef, employeeRef, incomesRef, paymentNoteRef, accountingRef, employeeActivity, rentsRef } from '../../utils/firebaseHelpers'
+import { expensesRef, employeeRef, incomesRef, paymentNoteRef, accountingRef, employeeActivity, rentsRef, rentalsRef } from '../../utils/firebaseHelpers'
 import { FETCH_PAYMENTS_NOTES } from './types'
 export const addCompanyExpense = (companyId, values) => 
     expensesRef(companyId).push(values)
     .then(() => updateAccountingCompany(companyId, values.mount, 'expenses'))
     .then(() => updateAccountingEmployee(companyId, values.uid, values.mount, 'expenses'))
+    .then(() => updateAccountingRental(companyId, values.rentalId, values.mount, 'expenses'))
     .then(() => addEmployeeActivity(companyId, values.uid, { 
         type: 'EXPENSES', 
         message: `Expended $${values.mount} in ${values.description}` }))
@@ -14,6 +15,7 @@ export const addCompanyIncome = (companyId, values) => {
     .then(() => updateAccountingCompany(companyId, values.mount, 'incomes'))
     .then(() => updateAccountingEmployee(companyId, values.uid, values.mount, 'incomes'))
     .then(() => updatePaymentDate(companyId, values.rentalId, values.quantity))
+    .then(() => updateAccountingRental(companyId, values.rentalId, values.mount, 'incomes'))
     .then(() => addEmployeeActivity(companyId, values.uid, { 
         type: 'PAYMENT', 
         message: `Payment from ${values.tenant} of $${values.mount} for ${values.quantity} months`}))
@@ -58,12 +60,15 @@ const updatePaymentDate = (companyId, rentalId, months) => rentsRef(companyId).c
         rentsRef(companyId).child(rentalId).update({ paymentDate: snap.val().paymentDate + 1000 * 60 * 60 * 24 * 30 * months }))
 
 const updatePaymentDateByDays = (companyId, rentalId, days) => rentsRef(companyId).child(rentalId)
-.once('value', snap =>  
+    .once('value', snap =>  
     rentsRef(companyId).child(rentalId).update({ paymentDate: snap.val().paymentDate + 1000 * 60 * 60 * 24 * days }))
 
 const addPaymentNoteToRent = (companyId, rentalId, paymentNote) => rentsRef(companyId).child(rentalId).update({ paymentNote }) 
 
 const updateAccountingCompany = (companyId, mount, type) => accountingRef(companyId).child(type)
+    .transaction(transaction => transaction += Number(mount))
+
+const updateAccountingRental = (companyId, mount, rentalId, type) => rentalsRef(companyId).child(rentalId).child(type)
     .transaction(transaction => transaction += Number(mount))
 
 const updateAccountingEmployee = (companyId, uid, mount, type) => employeeRef(companyId, uid).child(type)
