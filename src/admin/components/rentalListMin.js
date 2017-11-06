a621bc43eafcd723c7e9d0695b2b1cd30f829432
Steven Anderson <sdandersonz97@gmail.com>
2017-11-06 03:23:09 -0400

    prettier

--- src/admin/components/rentalListMin.js
+++ src/admin/components/rentalListMin.js
@@ -1,22 +1,24 @@
-import React from 'react'
-import { Card, CardBody, CardHeader, Table, TableBody } from '../../common'
+import React from "react";
+import { Card, CardBody, CardHeader, Table, TableBody } from "../../common";
 
-const RentalListMin = ({ render, renderTableHeader, onClickChangeScreen }) => { 
-    return( 
-        <Card>
-            <CardHeader title='Rentals' category='All the rentals'/>                               
-            <CardBody>
-                <Table>
-                    {renderTableHeader()}
-                    <TableBody>
-                        { render() }
-                    </TableBody>
-                </Table>
-                <button className='btn btn-primary' onClick={() => onClickChangeScreen('employees')}>Next</button>
-            </CardBody>
-        </Card>
-    )
-}
+const RentalListMin = ({ render, renderTableHeader, onClickChangeScreen }) => {
+  return (
+    <Card>
+      <CardHeader title="Rentals" category="All the rentals" />
+      <CardBody>
+        <Table>
+          {renderTableHeader()}
+          <TableBody>{render()}</TableBody>
+        </Table>
+        <button
+          className="btn btn-primary"
+          onClick={() => onClickChangeScreen("employees")}
+        >
+          Next
+        </button>
+      </CardBody>
+    </Card>
+  );
+};
 
-
-export default RentalListMin
+export default RentalListMin;
